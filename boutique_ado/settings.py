5881456ef43049325bdc6dc55cfbca025fad8826
FlorianS4 <florian.sassmann@hotmail.com>
2025-02-21 16:56:03 +0100

    fix: tip error fix and correctly linkin for templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
-            'buildins': [
+            'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
                 'crispy_forms.templatetags.crispy_forms_field',
             ]
